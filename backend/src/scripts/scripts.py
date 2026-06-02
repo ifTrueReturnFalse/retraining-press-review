bb456781c4e61db6dc589924203238c713ce9261
Timothé Duquesne <timothe.duquesne@gmail.com>
2026-06-02 18:25:34 +0200

    update: when starting the db container, now check if it already exist

--- backend/src/scripts/scripts.py
+++ backend/src/scripts/scripts.py
@@ -4,22 +4,34 @@ from config import settings
 
 
 def start_db():
-    cmd = [
-        "docker",
-        "run",
-        "--name",
-        "newsfoundry-db",
-        "-e",
-        f"POSTGRES_USER={settings.DATABASE_USER}",
-        "-e",
-        f"POSTGRES_PASSWORD={settings.DATABASE_PASSWORD}",
-        "-e",
-        f"POSTGRES_DB={settings.DATABASE}",
-        "-p",
-        "5432:5432",
-        "-d",
-        "postgres:17",
-    ]
+    container_name = "newsfoundry-db"
+
+    # Check if container exists
+    check_cmd = ["docker", "ps", "-a", "-q", "-f", f"name=^/{container_name}$"]
+    container_exists = subprocess.run(check_cmd, capture_output=True, text=True).stdout.strip()
+    print(container_exists)
+
+    if container_exists:
+        print(f"Starting the existing container : {container_name}")
+        cmd = ["docker", "start", container_name]
+    else:
+        print(f"Creating and lauching the container : {container_name}")
+        cmd = [
+            "docker",
+            "run",
+            "--name",
+            "newsfoundry-db",
+            "-e",
+            f"POSTGRES_USER={settings.DATABASE_USER}",
+            "-e",
+            f"POSTGRES_PASSWORD={settings.DATABASE_PASSWORD}",
+            "-e",
+            f"POSTGRES_DB={settings.DATABASE}",
+            "-p",
+            "5432:5432",
+            "-d",
+            "postgres:17",
+        ]
 
     result = subprocess.run(cmd)
     sys.exit(result.returncode)
